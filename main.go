c3ce9d74c7d0323417c583b36acea72a84c0c78d
Pascal Keller <jaures@gmx.de>
2026-01-04 10:26:10 +0100

    main.go editet

--- main.go
+++ main.go
@@ -6,8 +6,17 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	// Create command registry
+	commands := getCommands()
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -21,7 +30,54 @@ func main() {
 			continue
 		}
 
-		firstWord := cleaned[0]                         // Nimmt das ERSTE Element des Slice
-		fmt.Printf("Your command was: %s\n", firstWord) // Gibt nur das erste Wort aus
+		commandName := cleaned[0]
+
+		// Look up command in registry
+		command, exists := commands[commandName]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		// Execute the command callback
+		err := command.callback()
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
 	}
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("\nWelcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	commands := getCommands()
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
