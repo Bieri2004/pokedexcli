7ef1806878c3bc12ba6776d3df06c2611ebaf8bb
Pascal Keller <jaures@gmx.de>
2026-01-04 09:58:03 +0100

    added REPL

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		text := scanner.Text()
+
+		cleaned := cleanInput(text)
+
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		firstWord := cleaned[0]                         // Nimmt das ERSTE Element des Slice
+		fmt.Printf("Your command was: %s\n", firstWord) // Gibt nur das erste Wort aus
+	}
 }
